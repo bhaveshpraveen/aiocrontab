e261129cd9b97ff28ea5dd41813be9624fa950bd
Bhavesh <bhavesh@moms-MacBook-Pro.local>
2020-05-15 09:21:56 +0530

    get initial tests working

--- tests/test_aiocrontab.py
+++ tests/test_aiocrontab.py
@@ -1,5 +1,19 @@
 from aiocrontab import __version__
+from aiocrontab.crontab import crontab
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_task_is_called_when_decorated_with_crontab(mocker):
+
+    task = mocker.Mock(name="task", return_value=True)
+
+    decorated_task = crontab('* * * * *')(task)
+    ret = decorated_task("test1", "test2")
+
+    task.assert_called_with("test1", "test2")
+    assert task.call_count == 1
+
+    assert ret is True
