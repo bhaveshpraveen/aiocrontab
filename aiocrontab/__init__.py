dbb7fbdb8469e56e366e6ce132df5fc8e7250225
bhavesh <bhaveshpraveen10@gmail.com>
2020-05-19 05:13:52 +0530

    add __all__ for the lib

--- aiocrontab/__init__.py
+++ aiocrontab/__init__.py
@@ -1,1 +1,13 @@
 __version__ = "0.1.0"
+
+from aiocrontab.core import Crontab
+
+_aiocrontab = Crontab()
+register = _aiocrontab.register
+run = _aiocrontab.run
+
+__all__ = [
+    "register",
+    "run",
+    "Crontab",
+]
