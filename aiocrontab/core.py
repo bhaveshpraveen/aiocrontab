5b05d6c568ecbb71460a142621286f369c8ace46
bhavesh <bhaveshpraveen10@gmail.com>
2020-05-18 12:18:46 +0530

    add Crontab class

--- aiocrontab/core.py
+++ aiocrontab/core.py
@@ -120,11 +120,115 @@ async def handle_cronjob(
 
 
 def func(id: int) -> None:
-    print(f"{time.ctime()}:[Task: {id}] Block Sleeping for 300 secs")
+    logging.info(f"[Task: {id}] Block Sleeping for 300 secs")
     time.sleep(300)
-    print(
-        f"{time.ctime()}: [Task: {id}] Block Sleeping Finished for 300 secs."
-    )
+    logging.info(f"[Task: {id}] Block Sleeping Finished for 300 secs.")
+
+
+# specifies the default error signals to handle/intercept for graceful shutdown
+_DEFAULT_ERROR_SIGNALS = [signal.SIGINT]
+
+
+class Crontab:
+    def __init__(
+        self,
+        loop: Optional[asyncio.AbstractEventLoop] = None,
+        executor: Optional[thread.ThreadPoolExecutor] = None,
+        error_signals_to_intercept: Optional[List[TSignal]] = None,
+    ):
+        self.registered_tasks = []
+        self._loop = loop
+        self._executor = executor
+        self.error_signals_to_intercept = (
+            error_signals_to_intercept or _DEFAULT_ERROR_SIGNALS
+        )
+
+    @property
+    def loop(self):
+        if self._loop is None:
+            self._loop = asyncio.get_event_loop()
+            self._loop.set_exception_handler(self.handle_exception)
+        elif self._loop.is_closed():
+            self._loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self._loop)
+            self._loop.set_exception_handler(self.handle_exception)
+        return self._loop
+
+    @property
+    def executor(self):
+        if self._executor is None:
+            self._executor = thread.ThreadPoolExecutor()
+        return self._executor
+
+    def register(self, pattern: str):
+        def decorator(f: Callable):
+            task_to_register = TRegisteredTask(pattern=pattern, func=f)
+            self.registered_tasks.append(task_to_register)
+            return f
+
+        return decorator
+
+    async def shutdown(self, signal: Optional[TSignal] = None):
+        if signal:
+            logging.info("Received exit signal %s", signal)
+        tasks = [
+            t for t in asyncio.all_tasks() if t is not asyncio.current_task()
+        ]
+        for task in tasks:
+            task.cancel()
+
+        logging.info("Cancelling tasks...")
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+        # waits until all the background tasks running in different threads to complete.
+        logging.info("Waiting for background threads to finish..")
+        self.executor.shutdown(wait=True)
+        self.loop.stop()
+
+    def handle_error_signals(
+        self,
+        loop: asyncio.AbstractEventLoop,
+        error_signals: Optional[List[TSignal]] = None,
+    ):
+        error_signals_to_handle = error_signals or _DEFAULT_ERROR_SIGNALS
+        for _signal in error_signals_to_handle:
+            loop.add_signal_handler(
+                _signal, lambda s=_signal: loop.create_task(self.shutdown(s))
+            )
+
+    def handle_exception(self, loop, context: Dict[str, Any]) -> None:
+        """Set handler as the new event loop exception handler.
+        :param loop: Event loop
+        :param context: Contains the details of the exception. Details about the context dict: https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.call_exception_handler
+        :return: None
+        """
+        msg = context.get("exception", context["message"])
+        logging.error("%s", msg)
+        logging.info("Shutting down..")
+        loop.create_task(self.shutdown())
+
+    def run(self):
+        try:
+            for registered_task in self.registered_tasks:
+                pattern, func = (
+                    registered_task["pattern"],
+                    registered_task["func"],
+                )
+                self.loop.create_task(
+                    (
+                        handle_cronjob(
+                            pattern=pattern,
+                            func=func,
+                            loop=self.loop,
+                            executor=self.executor,
+                        )
+                    )
+                )
+            self.loop.run_forever()
+        finally:
+            logging.info("Closing the loop.")
+            self.loop.close()
+            logging.info("Shutdown Aiocrontab successfully.")
 
 
 def main():
