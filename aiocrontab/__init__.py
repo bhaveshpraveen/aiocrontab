05b8ff952ccc98bf4a311ad0ea02dbcdd928444d
bhavesh <bhaveshpraveen10@gmail.com>
2020-05-31 07:13:34 +0530

    global aiocrontab instance uses `global.aiocrontab.core` as the logger name

--- aiocrontab/__init__.py
+++ aiocrontab/__init__.py
@@ -1,11 +1,13 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from aiocrontab.core import Crontab
+from aiocrontab.core import create_logger
 
 
-_aiocrontab = Crontab()
-register = _aiocrontab.register
-run = _aiocrontab.run
+__logger = create_logger(namespace="global.aiocrontab.core")
+__aiocrontab = Crontab(logger=__logger)
+register = __aiocrontab.register
+run = __aiocrontab.run
 
 __all__ = [
     "register",
